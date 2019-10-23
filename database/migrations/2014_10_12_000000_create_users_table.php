8f6057f61b07703634d37c166d0413596f490dc5
collmatt10 <matthewcollins@live.ie>
2019-10-23 01:05:06 +0100

    added username functionality and tested using tinker

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) { //describes structure of database
+            $table->bigIncrements('id'); //blueprint of database table
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('username')->unique();//unique is for security so things can only be used once and not cause problems
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+      <div class="col-3 p-5">
+<img src="https://scontent-frt3-1.cdninstagram.com/vp/79155e99f758275ac4233ad3e900bbbd/5E038B38/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=scontent-frt3-1.cdninstagram.com " class="rounded-circle">
+      </div>
+      <div class="col-9 p-5">
+        <div><h1>FreeCodeCamp</h1></div>
+          <div class="d-flex">
+            <div class="pr-5"><strong>153 </strong>posts </div>
+            <div class="pr-5"><strong>23k </strong>followers</div>
+            <div class="pr-5"><strong>212 </strong>following</div>
+          </div>
+
+            <div class="pr-4 pt-4 font-weight-bold">FreeCodeCamp.org </div>
+            <div class="pb-4">We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit. </div>
+            <div><a href="#">www.freecodegram.org</div>
+          </div>
+        </div>
+
+<div class="row pt-4">
+    <div class="col-4">
+        <img src="https://scontent-dub4-1.cdninstagram.com/vp/e56768940a57cd015327e11af99c35a1/5E415934/t51.2885-15/e35/c1.0.748.748a/s480x480/72129638_1162089590650919_686521508869216152_n.jpg?_nc_ht=scontent-dub4-1.cdninstagram.com&_nc_cat=111" class="w-100">
+    </div>
+    <div class="col-4">
+        <img src="https://scontent-dub4-1.cdninstagram.com/vp/3cec7031522273c8277791abf57fcd17/5E4D1419/t51.2885-15/sh0.08/e35/c0.109.925.925a/s640x640/71888114_384946162459981_3818877377020383907_n.jpg?_nc_ht=scontent-dub4-1.cdninstagram.com&amp;_nc_cat=101" class="w-100">
+    </div>
+    <div class="col-4">
+        <img src="https://scontent-dub4-1.cdninstagram.com/vp/e68d19a0f12f67973fe2b17c26528d0f/5E48B0B6/t51.2885-15/sh0.08/e35/c2.0.746.746a/s640x640/70188541_238534077124941_2455868037630971059_n.jpg?_nc_ht=scontent-dub4-1.cdninstagram.com&_nc_cat=109 " class="w-100">
+</div>
+    </div>
+   </div>
+
+
+@endsection
